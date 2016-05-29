ba59451a40aa3a8cf17f0fbf3d8c396ec8745ade
Bastian Schwarz <bastian@codename-php.de>
2016-05-30 00:00:35 +0200

    Added definitions provider and using PSR-7 interfaces

--- src/main/php/de/imxnet/imxplatformphp/http/DefinitionsProvider.php
+++ src/main/php/de/imxnet/imxplatformphp/http/DefinitionsProvider.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2014 Bastian Schwarz <bastian@codename-php.de>.
+ * Copyright 2016 Bastian Schwarz <schwarz@infomax-it.de>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,16 +14,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * @namespace
  */
-
 namespace de\imxnet\imxplatformphp\http;
 
 /**
+ * Provides defintions for the Http module
  *
- * @author Bastian Schwarz <bastian@codename-php.de>
+ * @author Bastian Schwarz <schwarz@infomax-it.de>
  */
-class TestCase extends \de\codenamephp\platform\test\TestCase {
-//put your code here
+class DefinitionsProvider implements \de\codenamephp\platform\di\definitionsProvider\iArray {
+
+  /**
+   *
+   * @return array
+   */
+  public function getDefinitions() {
+    return [
+      \Psr\Http\Message\ServerRequestInterface::class => \DI\factory(function () {
+            return \Zend\Diactoros\ServerRequestFactory::fromGlobals();
+          })
+    ];
+  }
 }
